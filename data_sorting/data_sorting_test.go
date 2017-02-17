36c0e02ea0dbcf78d68ce530afb4b0257341509d
Michael Christenson II <m3talsmith@gmail.com>
2017-02-16 22:40:09 -0700

    No use testing for nil The function will always return a []int whether it's empty or not. No need to test for nil.

--- data_sorting/data_sorting_test.go
+++ data_sorting/data_sorting_test.go
@@ -9,7 +9,7 @@ func TestFindDuplicatesNaive(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 5, 6, 7, 8, 1}
 	knownDuplicates := []int{1, 5}
 	duplicates := FindDuplicatesNaive(numbers)
-	if duplicates == nil {
+	if len(duplicates) == 0 {
 		t.Fatalf("No duplicates were found")
 	} else if !reflect.DeepEqual(duplicates, knownDuplicates) {
 		t.Fatalf("\nExpected: %v\nGot: %v", knownDuplicates, duplicates)
